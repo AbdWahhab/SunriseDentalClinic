72d820858fbd53d981b1456ae30f4540916045d5
ABDUL WAHAB <ABDUL WAHAB@AbdulWahhab>
2026-09-05 11:38:57 +0530

    Move database password to environment variable

--- SunriseDentalClinic/src/main/java/com/sunrise/util/DBConnection.java
+++ SunriseDentalClinic/src/main/java/com/sunrise/util/DBConnection.java
@@ -9,23 +9,28 @@ public class DBConnection {
     private static final String URL =
             "jdbc:mysql://localhost:3306/sunrise_dental_db";
 
-    private static final String USERNAME = "root";
+    private static final String USER =
+            "root";
 
     private static final String PASSWORD =
-            "#FaizaShireen@10R";
+            System.getenv("SUNRISE_DB_PASSWORD");
 
-    public static Connection getConnection() throws SQLException {
-
+    static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new SQLException("MySQL JDBC Driver not found.", e);
+            throw new RuntimeException("MySQL JDBC Driver not found.", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+
+        if (PASSWORD == null || PASSWORD.isBlank()) {
+            throw new SQLException(
+                    "Environment variable SUNRISE_DB_PASSWORD is not configured."
+            );
         }
 
-        return DriverManager.getConnection(
-                URL,
-                USERNAME,
-                PASSWORD
-        );
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
